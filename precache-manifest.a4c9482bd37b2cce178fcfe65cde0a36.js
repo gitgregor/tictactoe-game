4766d5b3ec27812335c81b4e6939d54846a74ca6
Gregory <gerda.trust@gmail.com>
2020-02-01 19:04:01 +0100

    Updates

---
+++ precache-manifest.a4c9482bd37b2cce178fcfe65cde0a36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d726f59a7af288f0f8b85620a80638",
+    "url": "/tictactoe-game/index.html"
+  },
+  {
+    "revision": "0a5bfbb3ad5ac9ae4759",
+    "url": "/tictactoe-game/static/css/main.747f4c3e.chunk.css"
+  },
+  {
+    "revision": "23cdd8d8ab692e8fc7fe",
+    "url": "/tictactoe-game/static/js/2.ffcc1fad.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tictactoe-game/static/js/2.ffcc1fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a5bfbb3ad5ac9ae4759",
+    "url": "/tictactoe-game/static/js/main.9cb5f0c3.chunk.js"
+  },
+  {
+    "revision": "a8b3e4d0dc438f48539c",
+    "url": "/tictactoe-game/static/js/runtime-main.71553041.js"
+  },
+  {
+    "revision": "ae445d458a363359be9289624e287734",
+    "url": "/tictactoe-game/static/media/seascape.ae445d45.jpg"
+  },
+  {
+    "revision": "8c511c2e73b7bf4a4e86e19b551dadc3",
+    "url": "/tictactoe-game/static/media/splashing_640.8c511c2e.jpg"
+  }
+]);
